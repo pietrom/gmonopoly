db6e930dd738a907c654a6579b3beff84b50281e
Pietro Martinelli <amicofragile@gmail.com>
2014-12-28 00:51:12 +0100

    Play twenty rounds maintaining Players' order. In hundred games with two Players both orders occur

--- src/main/groovy/org/amicofragile/gmonopoly/MonopolyGame.groovy
+++ src/main/groovy/org/amicofragile/gmonopoly/MonopolyGame.groovy
@@ -3,7 +3,7 @@ package org.amicofragile.gmonopoly
 class MonopolyGame {
 	private Board board
 	private List players
-	
+
 	def MonopolyGame(Board board, List players) {
 		if(players.size() < 2 || players.size() > 8) {
 			throw new IllegalArgumentException("Can't play with lesser than two or more than eight Players")
@@ -12,6 +12,13 @@ class MonopolyGame {
 		this.players = players
 		players.each {
 			it.location = board.first
+		}
+	}
+
+	def play() {
+		Collections.shuffle(players)
+		20.times {
+			players.each { it.roll(7) }
 		}
 	}
 }
--- src/test/groovy/org/amicofragile/gmonopoly/MonopolyGameTest.groovy
+++ src/test/groovy/org/amicofragile/gmonopoly/MonopolyGameTest.groovy
@@ -38,4 +38,61 @@ class MonopolyGameTest {
 		}
 		def game = new MonopolyGame(board, players)
 	}
+	
+	@Test
+	void everyPlayerPlaysTwentyRounds() {
+		def board = new Board()
+		def players = []
+		def horse = new Player(name : "Horse")
+		def horseRoundCount = 0
+		horse.metaClass.roll = { int dicesValue ->
+			horseRoundCount++
+		}
+		players.add(horse)
+		
+		def cat = new Player(name : "Cat")
+		def catRoundCount = 0
+		cat.metaClass.roll = { int dicesValue ->
+			catRoundCount++
+		}
+		players.add(cat)
+		def game = new MonopolyGame(board, players)
+		game.play()
+		assertEquals(20, horseRoundCount)
+		assertEquals(20, catRoundCount)
+	}
+	
+	@org.junit.Test
+	void givenTwoPlayersInHundredGamesThanBothPossibleOrdersOccur() {
+		def accumulator = ""
+		def pl1 = new Player(name : "PL1")
+		pl1.metaClass.roll = { int dicesValue ->
+			accumulator += "1"
+		}
+		def pl2 = new Player(name : "PL2")
+		pl2.metaClass.roll = { int dicesValue ->
+			accumulator += "2"
+		}
+		def players = []
+		players.add(pl1)
+		players.add(pl2)
+		
+		def order12occurred = false
+		def order21occurred = false
+		100.times {
+			def game = new MonopolyGame(new Board(), players)
+			accumulator = ""
+			game.play()
+			if(accumulator.equals("12" * 20)) {
+				order12occurred = true
+			} else if (accumulator.equals("21" * 20)) {
+				order21occurred = true
+			} else {
+			println accumulator
+				fail("Order cant'change during the game")
+			}
+		}
+		assertTrue(order12occurred)
+		assertTrue(order21occurred)
+	}
 }
