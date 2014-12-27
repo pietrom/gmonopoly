503c3604e657bd40f080eddcdac5f3b9826e9927
Pietro Martinelli <amicofragile@gmail.com>
2014-12-27 23:22:15 +0100

    A BoardLocation points to a 'next' location

--- src/main/groovy/org/amicofragile/gmonopoly/BoardLocation.groovy
+++ src/main/groovy/org/amicofragile/gmonopoly/BoardLocation.groovy
@@ -2,7 +2,12 @@ package org.amicofragile.gmonopoly
 
 class BoardLocation {
 	def name
+	def next
 	def BoardLocation(String name) {
 		this.name = name
+	}
+	
+	def next() {
+		next
 	}
 }
--- src/test/groovy/org/amicofragile/gmonopoly/BoardLocationTest.groovy
+++ src/test/groovy/org/amicofragile/gmonopoly/BoardLocationTest.groovy
@@ -9,4 +9,12 @@ class BoardLocationTest {
 		def location = new BoardLocation(name)
 		assertEquals(name, location.name)
 	}
+	
+	@org.junit.Test
+	void boardLocationHasNext() {
+		def loc1 = new BoardLocation("L1")
+		def loc2 = new BoardLocation("L2")
+		loc1.next = loc2
+		assertEquals(loc2, loc1.next())
+	}
 }
